327364f7637ea00dd60e7e62009d6794a8bd7110
Caleb <calebfunderburk@icloud.com>
2022-09-26 15:58:00 -0400

    connected server to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -6,10 +7,24 @@ const app = express()
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Zubat#41',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
+
 app.use((req, res) => {
     res.status(404).end()
 })
 
 app.listen(PORT, () => {
-    console.log(`Server runnong on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
